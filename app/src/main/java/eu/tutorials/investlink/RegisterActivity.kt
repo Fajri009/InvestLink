bb3fd41dd23d38ddb8edd3fe385775cf1390b521
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2023-03-24 10:23:47 +0700

    Sync firebase to the project and adding login and register page

--- app/src/main/java/eu/tutorials/investlink/RegisterActivity.kt
+++ app/src/main/java/eu/tutorials/investlink/RegisterActivity.kt
@@ -2,13 +2,10 @@ package eu.tutorials.investlink
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var auth : FirebaseAuth
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
