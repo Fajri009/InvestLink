1f277cf460500cfe9a775768dfc9713a0fb9408d
Finn <bagusalfin50@gmail.com>
2023-03-24 16:13:27 +0700

    add icon mata di bagian password dan code di login activity

--- app/src/main/java/eu/tutorials/investlink/LoginActivity.kt
+++ app/src/main/java/eu/tutorials/investlink/LoginActivity.kt
@@ -1,11 +1,80 @@
 package eu.tutorials.investlink
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    /**Inisialisasi**/
+    lateinit var getEmail: EditText
+    lateinit var getPassword: EditText
+    lateinit var daftar: EditText
+    lateinit var btnLogin: Button
+    lateinit var waiting: ProgressDialog
+    var firebaseAuth = FirebaseAuth.getInstance()
+
+    override fun onStart() {
+        super.onStart()
+        if (firebaseAuth.currentUser!==null){
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+    /**InCommit method, supaya tidak usah mengetik getID secara terus menerus**/
+    fun inCommit(){
+        getEmail = findViewById(R.id.email)
+        getPassword = findViewById(R.id.pass)
+        daftar = findViewById(R.id.daftar)
+        btnLogin = findViewById(R.id.loginBtn)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        /**Panggil ID**/
+        inCommit()
+
+        /**Loading**/
+        waiting = ProgressDialog(this)
+        waiting.setTitle("Logging")
+        waiting.setMessage("Tunggu Sebentar...")
+
+        /**Jika Button di click dan email dan password sudah terisi maka jalankan method**/
+        btnLogin.setOnClickListener{
+            if (getEmail.text.isNotEmpty() && getPassword.text.isNotEmpty()){
+                loginAct()
+            }else{
+                Toast.makeText(this,"Isi email dan password dulu",LENGTH_SHORT).show()
+            }
+        }
+        daftar.setOnClickListener{
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+    /**Login Activity**/
+    private fun loginAct(){
+        val email = getEmail.text.toString()
+        val password = getPassword.text.toString()
+
+        waiting.show()
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                startActivity(Intent(this,MainActivity::class.java))
+            }
+            .addOnFailureListener{
+                error -> Toast.makeText(this,error.localizedMessage, LENGTH_SHORT).show()
+
+            }
+            .addOnCompleteListener{
+                waiting.dismiss()
+            }
+
     }
 }
