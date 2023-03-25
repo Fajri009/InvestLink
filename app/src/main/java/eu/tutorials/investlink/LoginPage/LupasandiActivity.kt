7e3527c22af257f75bd2a41b4ec274a38f77d04d
Finn <bagusalfin50@gmail.com>
2023-03-25 12:41:50 +0700

    add toolbar di login dan lupa sandi page

--- app/src/main/java/eu/tutorials/investlink/LoginPage/LupasandiActivity.kt
+++ app/src/main/java/eu/tutorials/investlink/LoginPage/LupasandiActivity.kt
@@ -5,36 +5,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import eu.tutorials.investlink.R
 
-
 class LupasandiActivity : AppCompatActivity() {
-//    private lateinit var getEmailVerif: EditText
-//    private lateinit var verifBtn: Button
-//
-//    fun inCommit(){
-//        getEmailVerif = findViewById(R.id.email_verif)
-//        verifBtn = findViewById(R.id.verifBtn)
-//    }
+    private lateinit var getEmail: EditText
+    private lateinit var getPhoneNumber: EditText
+    private lateinit var backPage: ImageView
+    private lateinit var verifBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lupasandi)
 
-//        inCommit()
-//
-//        verifBtn.setOnClickListener{
-//            if (getEmailVerif.text.isNotEmpty()){
-//                verifAct()
-//            }else{
-//                Toast.makeText(this,"Isi emaildulu", Toast.LENGTH_SHORT).show()
-//            }
-//        }
-//
-//        verifAct()
+        getEmail = findViewById(R.id.email_verif)
+        getPhoneNumber = findViewById(R.id.phone_verif)
+        backPage = findViewById(R.id.backPage)
+        verifBtn = findViewById(R.id.btnVerif)
+
+        verifBtn.setOnClickListener {
+            if (getEmail.text.isNotEmpty() && getPhoneNumber.text.isNotEmpty()){
+
+            }else{
+                Toast.makeText(this,"Masukkan Email dan Nomer Telepon dulu",LENGTH_SHORT).show()
+            }
+        }
+        goBack()
+
     }
-//    private fun verifAct() {
-//        startActivity(Intent(this,LoginActivity::class.java))
-//    }
+
+    private fun goBack() {
+        backPage.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+        }
+    }
 }
